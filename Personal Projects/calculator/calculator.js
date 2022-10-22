a6d7024cf9789e27a430ef2e2fe644d561784338
NovellePlayz <114629678+NovellePlayz@users.noreply.github.com>
2022-10-21 20:12:44 -0400

    Add files via upload

---
+++ Personal Projects/calculator/calculator.js
@@ -0,0 +1,7 @@
+const numbers = document.getElementsByClassName(".numbers");
+const decimal = document.getElementsByClassName(".decimal");
+const operation = document.getElementsByClassName(".operation");
+const clear = document.getElementsByClassName(".clear");
+const equal = document.getElementsByClassName(".equal");
+let cat = 1;
+console.log(cat);
